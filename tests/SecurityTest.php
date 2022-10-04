311a59752812104bebee3f3e19d0e321ac9ad0ff
Tom Broucke <tom@tombroucke.be>
2022-10-04 09:37:02 +0200

    added defender to security plugins list

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -5,7 +5,8 @@ use PHPUnit\Framework\TestCase;
 
 final class SecurityTest extends TestCase
 {
-    public function testIfHasDebugNotices() {
+    public function testIfHasDebugNotices()
+    {
         $security = new Security('production');
         ob_start();
         $security->debugNotice();
@@ -13,30 +14,34 @@ final class SecurityTest extends TestCase
         $this->assertNotEmpty($debugNotice);
         $this->assertStringContainsString('Disable debugging for better security', $debugNotice);
         $this->assertStringContainsString('Disallow file editing for better security', $debugNotice);
-        $this->assertStringContainsString(' Install & activate Wordfence or Sucuri Security for optimal security', $debugNotice);
+        $this->assertStringContainsString('Install & activate Wordfence, Sucuri Security or Defe for optimal security.', $debugNotice);
     }
     
-    public function testLoginHasGenericErrors() {
+    public function testLoginHasGenericErrors()
+    {
         $security = new Security('production');
         $this->assertNotEmpty($security->genericLoginErrors([]));
         $this->assertStringContainsString('Could not log you in', $security->genericLoginErrors([]));
         $this->assertStringContainsString('https://example.com/login', $security->genericLoginErrors([]));
     }
 
-    public function testIfAttachmentIsHttps() {
+    public function testIfAttachmentIsHttps()
+    {
         $security = new Security('production');
         $attachment = $security->forceAttachmentHttps('http://example.com/test.jpg');
         $this->assertEquals('https://example.com/test.jpg', $attachment);
     }
 
-    public function testIfCriticalOptionsCantBeUpdated() {
+    public function testIfCriticalOptionsCantBeUpdated()
+    {
         $security = new Security('production');
         $this->assertEquals(0, $security->disableUpdateCriticalOptions(1, 'users_can_register'));
         $this->assertEquals('subscriber', $security->disableUpdateCriticalOptions('administrator', 'default_role'));
         $this->assertEquals('other_value', $security->disableUpdateCriticalOptions('other_value', 'other_key'));
     }
 
-    public function testIfCriticalOptionsUpdateNoticeIsDisplayed() {
+    public function testIfCriticalOptionsUpdateNoticeIsDisplayed()
+    {
         global $pagenow;
         $pagenow = 'options-general.php';
         $security = new Security('production');
