caed0a73a2f1645d20ac12febbe06572e0e1de79
Tom Broucke <tom@tombroucke.be>
2022-10-14 16:25:59 +0200

    chore(bump): Bump version to 1.4.1

--- otomaties-core.php
+++ otomaties-core.php
@@ -6,7 +6,7 @@
  * Author URI:      https://tombroucke.be
  * Text Domain:     otomaties-core
  * Domain Path:     /lang
- * Version:           1.4.0
+ * Version:           1.4.1
  *
  * @package         Core
  */
