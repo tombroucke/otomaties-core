91251937efb73aa8e212572e9575920201fc10da
Tom Broucke <tom@tombroucke.be>
2023-10-17 00:00:51 +0200

    chore(bump): Bump version to 1.6.3

--- otomaties-core.php
+++ otomaties-core.php
@@ -6,7 +6,7 @@
  * Author URI:      https://tombroucke.be
  * Text Domain:     otomaties-core
  * Domain Path:     /lang
- * Version:           1.6.1
+ * Version:           1.6.3
  *
  * @package         Core
  */
