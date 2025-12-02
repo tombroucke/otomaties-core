c8f4ead37844bc8ff6564ed09f635a22e95dfc3b
Tom Broucke <tom@tombroucke.be>
2025-12-02 10:42:00 +0100

    💄 style: indentation

--- app/Modules/HealthTests/BlogIsPublic.php
+++ app/Modules/HealthTests/BlogIsPublic.php
@@ -23,10 +23,16 @@ class BlogIsPublic extends Abstracts\HealthTest
         $isProduction = otomatiesCore()->environment() === 'production';
 
         return $response
-            ->withLabel($isProduction ? __('Search engines can crawl the blog', 'otomaties-core') : __('Search engines are discouraged from crawling the blog', 'otomaties-core'))
+            ->withLabel(
+                $isProduction ?
+                __('Search engines can crawl the blog', 'otomaties-core') :
+                __('Search engines are discouraged from crawling the blog', 'otomaties-core')
+            )
             ->withDescription(sprintf(
                 '<p>%s</p>',
-                $isProduction ? __('Search engines can crawl the blog', 'otomaties-core') : __('Search engines are discouraged from crawling the blog', 'otomaties-core')
+                $isProduction ?
+                    __('Search engines can crawl the blog', 'otomaties-core') :
+                    __('Search engines are discouraged from crawling the blog', 'otomaties-core')
             ));
     }
 
@@ -45,7 +51,7 @@ class BlogIsPublic extends Abstracts\HealthTest
                 '<p>%s</p>',
                 sprintf(
                     $isProduction ?
-                        __('Search engines are discouraged from crawling the blog. Visit %s to change this setting', 'otomaties-core') :
+                        __('Search engines are discouraged from crawling the blog. Visit %s to change this setting', 'otomaties-core') : // phpcs:ignore Generic.Files.LineLength
                         __('Search engines can crawl the blog. Visit %s to change this setting', 'otomaties-core'),
                     sprintf(
                         '<a href="%s" target="_blank">%s</a>',
