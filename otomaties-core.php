280938087eb7a0d6e0e80deba8767d0a2d88ed7d
Tom Broucke <tom@tombroucke.be>
2025-12-10 10:20:32 +0100

    🔼 chore(bump): Bump version to 2.0.0

--- otomaties-core.php
+++ otomaties-core.php
@@ -9,7 +9,7 @@
  * Text Domain:     otomaties-core
  * Domain Path:     resources/languages
  * Requires PHP:    8.0
- * Version:           2.0.0-beta6
+ * Version:           2.0.0
  */
 
 // If this file is called directly, abort.
