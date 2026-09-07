76999388df652c3551659c5d409a47993d904fe7
Tom Broucke <tom@tombroucke.be>
2026-09-07 16:07:15 +0200

    🔼 chore(bump): Bump version to 2.3.1

--- otomaties-core.php
+++ otomaties-core.php
@@ -12,7 +12,7 @@ use Otomaties\Core\Plugin;
  * Text Domain:     otomaties-core
  * Domain Path:     resources/languages
  * Requires PHP:    8.0
- * Version:           2.3.0
+ * Version:           2.3.1
  */
 
 // If this file is called directly, abort.
