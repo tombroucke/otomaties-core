7b4474d0bab17c118cc8c5ceaa52953c858ce3e0
Tom Broucke <tom@tombroucke.be>
2026-09-07 16:09:01 +0200

    💄 style: pint

--- app/Helpers/WpEnvironment.php
+++ app/Helpers/WpEnvironment.php
@@ -4,13 +4,13 @@ namespace Otomaties\Core\Helpers;
 
 class WpEnvironment
 {
-	public static function defined(): bool
-	{
-		return defined('WP_ENV') && is_string(constant('WP_ENV'));
-	}
+    public static function defined(): bool
+    {
+        return defined('WP_ENV') && is_string(constant('WP_ENV'));
+    }
 
-	public static function get(): string
-	{
-		return self::defined() ? constant('WP_ENV') : 'production';
-	}
+    public static function get(): string
+    {
+        return self::defined() ? constant('WP_ENV') : 'production';
+    }
 }
